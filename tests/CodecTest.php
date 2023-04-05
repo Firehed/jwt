0f49c564f5483d2fc5a420510ee77862b49f9a9b
Eric Stern <eric@ericstern.com>
2023-04-05 15:53:46 -0700

    Add and test codec (#11)
    This is intended as a convenience tool to avoid having to work with too
    many classes in too many places. It aims to simplify the end-user API.
    See #10.

---
+++ tests/CodecTest.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Firehed\JWT;
+
+use Firehed\Security\Secret;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers Codec
+ */
+class CodecTest extends TestCase
+{
+    private Codec $codec;
+    private KeyContainer $container;
+
+    public function setUp(): void
+    {
+        $keyContainer = new KeyContainer();
+        $keyContainer->addKey('a', Algorithm::HMAC_SHA_256, new Secret('key-a'));
+        $keyContainer->addKey('b', Algorithm::HMAC_SHA_384, new Secret('key-b'));
+        $keyContainer->addKey('c', Algorithm::HMAC_SHA_512, new Secret('key-c'));
+        $this->codec = new Codec($keyContainer);
+        $this->container = $keyContainer;
+    }
+
+    public function testRoundtrip(): void
+    {
+        $claims = [
+            Claim::ISSUED_AT => time(),
+        ];
+        $encoded = $this->codec->encode($claims);
+        $decoded = $this->codec->decode($encoded);
+
+        self::assertSame($claims, $decoded->getClaims());
+        self::assertSame('c', $decoded->getKeyID());
+    }
+
+    public function testRoundtripWithSpecifiedKeyId(): void
+    {
+        $this->container->setDefaultKey('a');
+        $claims = [
+            Claim::ISSUED_AT => time(),
+        ];
+        $encoded = $this->codec->encode($claims, 'b');
+        $decoded = $this->codec->decode($encoded);
+
+        self::assertSame($claims, $decoded->getClaims());
+        self::assertSame('b', $decoded->getKeyID());
+    }
+
+    public function testRoundtripWithContainerDefaultKeyId(): void
+    {
+        $this->container->setDefaultKey('a');
+        $claims = [
+            Claim::ISSUED_AT => time(),
+        ];
+        $encoded = $this->codec->encode($claims);
+        $decoded = $this->codec->decode($encoded);
+
+        self::assertSame($claims, $decoded->getClaims());
+        self::assertSame('a', $decoded->getKeyID());
+    }
+}
